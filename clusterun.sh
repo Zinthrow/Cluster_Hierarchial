d0bc25e07f36af185f3593d0bffb4b106386f2d0
Zinthrow <33006657+Zinthrow@users.noreply.github.com>
2017-12-05 15:57:15 -0600

    Update clusterun.sh

--- clusterun.sh
+++ clusterun.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-# Uncomment the line below if you write a Java program
-#java cluster $1 $2 $3
-
-# Uncomment the line below for a Python program
-python3 Cluster.py data_file link_type k
-
-# Uncomment the line below to run a compiled C or C++ program
-#./cluster $1 $2 $3
-
-# Uncomment the line below for a Perl program
-#perl cluster.pl $1 $2 $3
-
-# Uncomment the line below for an R program
-#Rscript cluster.R $1 $2 $3
+python3 Cluster.py tiny-yeast.tsv A 4
